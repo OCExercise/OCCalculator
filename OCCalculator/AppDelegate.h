b7fbd6c793aaf0b3ace7834f730969841b5d78d7
Prez Cannady <revprez@opencorrelate.org>
2019-02-24 23:24:38 -0500

    Initial Commit

---
+++ OCCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OCCalculator
+//
+//  Created by Prez Cannady Jr on 2/24/19.
+//  Copyright © 2019 Open Correlate. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OCCalculator/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  OCCalculator
+//
+//  Created by Prez Cannady Jr on 2/24/19.
+//  Copyright © 2019 Open Correlate. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
